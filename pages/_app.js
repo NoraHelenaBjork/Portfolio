694b8f02702f2f3f216f91f2f0bd3eb01abf64ca
Nora Björk <norabjork@MacBook-Air.local>
2024-04-25 20:09:02 +0200

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
-import React, { useEffect } from 'react';
-import '../styles/globals.scss';
+import React, { useEffect } from "react";
+import "../styles/globals.scss";
 import DiamondLoader from "../components/utils/diamondLoader";
 import { keyframes, css } from "styled-components";
+import TagManager from "react-gtm-module";
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
@@ -16,39 +17,49 @@ export default function MyApp({ Component, pageProps }) {
   `;
 
   function stopLoading() {
-    const loader = document.getElementById('globalLoader');
+    const loader = document.getElementById("globalLoader");
     if (loader) {
-
       // Then hide it after animation
       setTimeout(() => {
-        loader.classList.add('fade-out');
+        loader.classList.add("fade-out");
         //loader.style.display = 'none';
       }, 1650);
 
       setTimeout(() => {
-        loader.style.display = 'none';
+        loader.style.display = "none";
       }, 2150);
-
     }
   }
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       stopLoading();
     }
+  }, []);
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-WDP5XNQC" });
   }, []);
 
   return (
     <>
       <Head>
         <title>Nora Björk</title>
-        <meta name="description" content="Swedish full stack developer and startup founder" />
-        <link rel="icon" type="image/ico" sizes="32x32" href="/images/favicon.ico" />
+        <meta
+          name="description"
+          content="Swedish full stack developer and startup founder"
+        />
+        <link
+          rel="icon"
+          type="image/ico"
+          sizes="32x32"
+          href="/images/favicon.ico"
+        />
         <link rel="apple-touch-icon" type="image/png" href="appleIcon.png" />
-        <meta name='Nora Björk' content='Nora Björks website'></meta>
-        <meta name="theme-color" content="#11041D" ></meta>
+        <meta name="Nora Björk" content="Nora Björks website"></meta>
+        <meta name="theme-color" content="#11041D"></meta>
       </Head>
-      <div id='globalLoader'>
+      <div id="globalLoader">
         <DiamondLoader />
       </div>
       {getLayout(<Component {...pageProps} />)}
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -32,9 +32,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
